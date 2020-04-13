744a765d5f13482aadac759c41ba119a6dba91d6
aryaman1706 <agrover1_be19@thapar.edu>
2020-04-14 00:12:32 +0530

    about updated

--- app.js
+++
@@ -1,6 +0,0 @@
-var about=document.querySelector('#about');
-if(window.screen.width<1000){
-    about.classList.remove("offset-s6");
-    about.classList.add("m6");
-    console.log(about.classList);
-}
